a8f23b1c6565b011cb0770498627bc80c5e1829f
Andrew Lee <andyrewlee@gmail.com>
2016-10-25 23:35:01 -0700

    Refactor login vc

--- Nooma/CurrentUser.swift
+++ Nooma/CurrentUser.swift
@@ -10,18 +10,18 @@ import Foundation
 
 let CurrentUser: [String: Any] = [
     "can_give_nooma": true,
-    "classroom": "NickWalter",
+    "classroom": "andrew",
     "classroom_id": 1,
     "country": "US",
     "id": 1,
-    "iframe": "https://www.filestackapi.com/api/file/KXOQa8UTbS3VQfVz1Tkw",
-    "image_url": "https://noomatv.s3-us-west-1.amazonaws.com/users/1/profile",
+    "iframe": "https://www.youtube.com/watch?v=dikRszdjGvA",
+    "image_url": "https://noomatv-development.s3-us-west-1.amazonaws.com/users/1/profile",
     "room": "",
     "status": [
         "is_fan": true,
-        "is_moderator": true,
-        "is_subscriber": false,
-        "is_teacher": false
+        "is_moderator": false,
+        "is_subscriber": true,
+        "is_teacher": true
     ],
     "study_room": false,
     "subscribers_only": false,
--- Nooma/LoginViewController.swift
+++ Nooma/LoginViewController.swift
@@ -18,19 +18,18 @@ class LoginViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.checkExistentSession()
-
-        let theme = A0Theme()
-        theme.registerImage(withName: "noomalogo", bundle: Bundle.main, forKey: A0ThemeIconImageName)
-        theme.register(UIColor(red:0.255, green:0.816, blue:0.478, alpha:1.00), forKey: A0ThemePrimaryButtonNormalColor)
-        theme.register(UIColor(red:0.455, green:1.000, blue:0.678, alpha:1.00), forKey: A0ThemePrimaryButtonHighlightedColor)
-        theme.register(.white, forKey: A0ThemePrimaryButtonTextColor)
-        theme.statusBarStyle = .lightContent
-        theme.register(.white, forKey: A0ThemeIconBackgroundColor)
-
-
-        A0Theme.sharedInstance().register(theme)
-      
+        
+        let existingSession = checkExistingSession()
+        
+        if existingSession {
+            performSegue(withIdentifier: "SignedInSegue", sender: nil)
+        } else {
+            promptLogin()
+        }
+    }
+    
+    private func promptLogin() {
+        setTheme()
         let lock = A0Lock()
         let controller = lock.newEmailViewController()
         lock.presentEmailController(controller, from: self)
@@ -41,41 +40,42 @@ class LoginViewController: UIViewController {
             
             defaults.set(NSKeyedArchiver.archivedData(withRootObject: profile), forKey: "profile")
             defaults.set(NSKeyedArchiver.archivedData(withRootObject: token), forKey: "token")
-            
             defaults.synchronize()
             
-            self.dismiss(animated: true, completion: nil)
+            
+            // Make call to back end
+            // GET /api/v1/login
+            // with header token and email
+            
+            self.dismiss(animated: false, completion: nil)
+            self.performSegue(withIdentifier: "SignedInSegue", sender: nil)
         }
     }
     
-    private func checkExistentSession() {
+
+    
+    private func checkExistingSession() -> Bool {
         let defaults = UserDefaults.standard
         
         if defaults.data(forKey: "token") != nil &&
             defaults.data(forKey: "profile") != nil {
-            
-            self.performSegue(withIdentifier: "SignedInSegue", sender: nil)
+            return true
         } else {
-            let lock = A0Lock()
-            let controller = lock.newEmailViewController()
-            lock.presentEmailController(controller, from: self)
-            
-            controller?.onAuthenticationBlock = { (profile, token) in
-                
-                let defaults = UserDefaults.standard
-                
-                defaults.set(NSKeyedArchiver.archivedData(withRootObject: profile), forKey: "profile")
-                defaults.set(NSKeyedArchiver.archivedData(withRootObject: token), forKey: "token")
-                defaults.synchronize()
-                
-                
-                // Make call to back end
-                // GET /api/v1/login
-                // with header token and email
-                
-                self.dismiss(animated: false, completion: nil)
-                self.performSegue(withIdentifier: "SignedInSegue", sender: nil)
-            }
+            return false
+
         }
+    }
+    
+    private func setTheme() {
+        let theme = A0Theme()
+        theme.registerImage(withName: "noomalogo", bundle: Bundle.main, forKey: A0ThemeIconImageName)
+        theme.register(UIColor(red:0.255, green:0.816, blue:0.478, alpha:1.00), forKey: A0ThemePrimaryButtonNormalColor)
+        theme.register(UIColor(red:0.455, green:1.000, blue:0.678, alpha:1.00), forKey: A0ThemePrimaryButtonHighlightedColor)
+        theme.register(.white, forKey: A0ThemePrimaryButtonTextColor)
+        theme.statusBarStyle = .lightContent
+        theme.register(.white, forKey: A0ThemeIconBackgroundColor)
+        
+        
+        A0Theme.sharedInstance().register(theme)
     }
 }
